290d9b66553ac7027064538118703b59fff25b55
Joachim Metz <joachim.metz@gmail.com>
2024-11-16 08:08:02 +0100

    Added support for lzvn decompression

--- src/compression/mod.rs
+++ src/compression/mod.rs
@@ -15,10 +15,12 @@ mod adc;
 mod bzip2;
 mod deflate;
 mod huffman;
+mod lzvn;
 mod traits;
 mod zlib;
 
 pub use adc::AdcContext;
 pub use bzip2::Bzip2Context;
 pub use deflate::DeflateContext;
+pub use lzvn::LzvnContext;
 pub use zlib::ZlibContext;
