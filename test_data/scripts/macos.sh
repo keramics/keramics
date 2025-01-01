2ce762aee827bc52ee7b59aa0200ea8ec35f1b5a
Joachim Metz <joachim.metz@gmail.com>
2025-01-01 11:54:07 +0100

    Updated copyright information

--- test_data/scripts/macos.sh
+++ test_data/scripts/macos.sh
@@ -2,7 +2,7 @@
 #
 # Script to generate Keramics test files on Mac OS.
 #
-# Copyright 2024 Joachim Metz <joachim.metz@gmail.com>
+# Copyright 2024-2025 Joachim Metz <joachim.metz@gmail.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License. You may
