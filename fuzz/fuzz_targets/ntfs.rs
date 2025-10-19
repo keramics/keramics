5d78eac0c6e9ff8c7439195e237e119809fc1065
Joachim Metz <joachim.metz@gmail.com>
2025-10-19 15:58:27 +0200

    Worked on OSS-Fuzz fuzz targets and code clean up

---
+++ fuzz/fuzz_targets/ntfs.rs
@@ -0,0 +1,29 @@
+/* Copyright 2024-2025 Joachim Metz <joachim.metz@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+
+use keramics_core::{DataStreamReference, open_fake_data_stream};
+use keramics_formats::ntfs::NtfsFileSystem;
+
+// New Technologies File System (NTFS) fuzz target.
+fuzz_target!(|data: &[u8]| {
+    let mut ntfs_file_system: NtfsFileSystem = NtfsFileSystem::new();
+
+    let data_stream: DataStreamReference = open_fake_data_stream(&data);
+    _ = ntfs_file_system.read_data_stream(&data_stream);
+
+    _ = ntfs_file_system.get_root_directory();
+});
