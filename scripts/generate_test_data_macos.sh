1259bbd1d472c567295a110b6a216b3305376b10
Joachim Metz <joachim.metz@gmail.com>
2026-07-14 10:25:41 +0200

    Worked on tools and code clean up

--- scripts/generate_test_data_macos.sh
+++ scripts/generate_test_data_macos.sh
@@ -45,88 +45,88 @@ assert_availability_binary()
 #
 create_file_entries()
 {
-MOUNT_POINT=$1
+    MOUNT_POINT=$1
 
-# Create an empty file
-touch ${MOUNT_POINT}/emptyfile
+    # Create an empty file
+    touch ${MOUNT_POINT}/emptyfile
 
-# Create a directory
-mkdir ${MOUNT_POINT}/testdir1
+    # Create a directory
+    mkdir ${MOUNT_POINT}/testdir1
 
-# Create a file
-echo "Keramics" > ${MOUNT_POINT}/testdir1/testfile1
+    # Create a file
+    echo "Keramics" > ${MOUNT_POINT}/testdir1/testfile1
 
-# Create a hard link to a file
-ln ${MOUNT_POINT}/testdir1/testfile1 ${MOUNT_POINT}/file_hardlink1
+    # Create a hard link to a file
+    ln ${MOUNT_POINT}/testdir1/testfile1 ${MOUNT_POINT}/file_hardlink1
 
-# Create a symbolic link to a file
-ln -s ${MOUNT_POINT}/testdir1/testfile1 ${MOUNT_POINT}/file_symboliclink1
+    # Create a symbolic link to a file
+    ln -s ${MOUNT_POINT}/testdir1/testfile1 ${MOUNT_POINT}/file_symboliclink1
 
-# Create a symbolic link to a directory
-ln -s ${MOUNT_POINT}/testdir1 ${MOUNT_POINT}/directory_symboliclink1
+    # Create a symbolic link to a directory
+    ln -s ${MOUNT_POINT}/testdir1 ${MOUNT_POINT}/directory_symboliclink1
 
-# Create a file with an UTF-8 NFC encoded filename
-touch `printf "${MOUNT_POINT}/nfc_t\xc3\xa9stfil\xc3\xa8"`
+    # Create a file with an UTF-8 NFC encoded filename
+    touch `printf "${MOUNT_POINT}/nfc_t\xc3\xa9stfil\xc3\xa8"`
 
-# Create a file with an UTF-8 NFD encoded filename
-touch `printf "${MOUNT_POINT}/nfd_te\xcc\x81stfile\xcc\x80"`
+    # Create a file with an UTF-8 NFD encoded filename
+    touch `printf "${MOUNT_POINT}/nfd_te\xcc\x81stfile\xcc\x80"`
 
-# Create a file with an UTF-8 NFD encoded filename
-touch `printf "${MOUNT_POINT}/nfd_\xc2\xbe"`
+    # Create a file with an UTF-8 NFD encoded filename
+    touch `printf "${MOUNT_POINT}/nfd_\xc2\xbe"`
 
-# Create a file with an UTF-8 NFKD encoded filename
-touch `printf "${MOUNT_POINT}/nfkd_3\xe2\x81\x844"`
+    # Create a file with an UTF-8 NFKD encoded filename
+    touch `printf "${MOUNT_POINT}/nfkd_3\xe2\x81\x844"`
 
-# Create a file with filename that requires case folding if
-# the file system is case-insensitive
-touch `printf "${MOUNT_POINT}/case_folding_\xc2\xb5"`
+    # Create a file with filename that requires case folding if
+    # the file system is case-insensitive
+    touch `printf "${MOUNT_POINT}/case_folding_\xc2\xb5"`
 
-# Create a file with a forward slash in the filename
-touch `printf "${MOUNT_POINT}/forward:slash"`
+    # Create a file with a forward slash in the filename
+    touch `printf "${MOUNT_POINT}/forward:slash"`
 
-# Create a symbolic link to a file with a forward slash in the filename
-ln -s ${MOUNT_POINT}/forward:slash ${MOUNT_POINT}/file_symboliclink2
+    # Create a symbolic link to a file with a forward slash in the filename
+    ln -s ${MOUNT_POINT}/forward:slash ${MOUNT_POINT}/file_symboliclink2
 
-# Create a file with a resource fork with content
-touch ${MOUNT_POINT}/testdir1/resourcefork1
-echo "My resource fork" > ${MOUNT_POINT}/testdir1/resourcefork1/..namedfork/rsrc
+    # Create a file with a resource fork with content
+    touch ${MOUNT_POINT}/testdir1/resourcefork1
+    echo "My resource fork" > ${MOUNT_POINT}/testdir1/resourcefork1/..namedfork/rsrc
 
-# Create a file with an extended attribute with content
-touch ${MOUNT_POINT}/testdir1/xattr1
-xattr -w myxattr1 "My 1st extended attribute" ${MOUNT_POINT}/testdir1/xattr1
+    # Create a file with an extended attribute with content
+    touch ${MOUNT_POINT}/testdir1/xattr1
+    xattr -w myxattr1 "My 1st extended attribute" ${MOUNT_POINT}/testdir1/xattr1
 
-# Create a directory with an extended attribute with content
-mkdir ${MOUNT_POINT}/testdir1/xattr2
-xattr -w myxattr2 "My 2nd extended attribute" ${MOUNT_POINT}/testdir1/xattr2
+    # Create a directory with an extended attribute with content
+    mkdir ${MOUNT_POINT}/testdir1/xattr2
+    xattr -w myxattr2 "My 2nd extended attribute" ${MOUNT_POINT}/testdir1/xattr2
 
-# Create a file with an extended attribute that is not stored inline
-read -d "" -n 8192 -r LARGE_XATTR_DATA < LICENSE
-touch ${MOUNT_POINT}/testdir1/large_xattr
-xattr -w mylargexattr "${LARGE_XATTR_DATA}" ${MOUNT_POINT}/testdir1/large_xattr
+    # Create a file with an extended attribute that is not stored inline
+    read -d "" -n 8192 -r LARGE_XATTR_DATA < LICENSE
+    touch ${MOUNT_POINT}/testdir1/large_xattr
+    xattr -w mylargexattr "${LARGE_XATTR_DATA}" ${MOUNT_POINT}/testdir1/large_xattr
 
-if test -x ${AFSCTOOL}
-then
-    # Create a file that uses HFS+ compression (decmpfs) compression method 3
-    echo "My compressed file" > ${MOUNT_POINT}/testdir1/compressed1
-    ${AFSCTOOL} -c -T ZLIB ${MOUNT_POINT}/testdir1/compressed1
+    if test -x ${AFSCTOOL}
+    then
+        # Create a file that uses HFS+ compression (decmpfs) compression method 3
+        echo "My compressed file" > ${MOUNT_POINT}/testdir1/compressed1
+        ${AFSCTOOL} -c -T ZLIB ${MOUNT_POINT}/testdir1/compressed1
 
-    # Create a file that uses HFS+ compression (decmpfs) compression method 4
-    ditto --nohfsCompression LICENSE ${MOUNT_POINT}/testdir1/compressed2
-    ${AFSCTOOL} -c -T ZLIB ${MOUNT_POINT}/testdir1/compressed2
+        # Create a file that uses HFS+ compression (decmpfs) compression method 4
+        ditto --nohfsCompression LICENSE ${MOUNT_POINT}/testdir1/compressed2
+        ${AFSCTOOL} -c -T ZLIB ${MOUNT_POINT}/testdir1/compressed2
 
-    # Create a file that uses HFS+ compression (decmpfs) compression method 7
-    echo "My compressed file" > ${MOUNT_POINT}/testdir1/compressed3
-    ${AFSCTOOL} -c -T LZVN ${MOUNT_POINT}/testdir1/compressed3
+        # Create a file that uses HFS+ compression (decmpfs) compression method 7
+        echo "My compressed file" > ${MOUNT_POINT}/testdir1/compressed3
+        ${AFSCTOOL} -c -T LZVN ${MOUNT_POINT}/testdir1/compressed3
 
-    # Create a file that uses HFS+ compression (decmpfs) compression method 8
-    ditto --nohfsCompression LICENSE ${MOUNT_POINT}/testdir1/compressed4
-    ${AFSCTOOL} -c -T LZVN ${MOUNT_POINT}/testdir1/compressed4
-fi
+        # Create a file that uses HFS+ compression (decmpfs) compression method 8
+        ditto --nohfsCompression LICENSE ${MOUNT_POINT}/testdir1/compressed4
+        ${AFSCTOOL} -c -T LZVN ${MOUNT_POINT}/testdir1/compressed4
+    fi
 
-# Note that compressed UDIF images don't allow for block or character device files.
+    # Note that compressed UDIF images don't allow for block or character device files.
 
-# Create a pipe (FIFO) file
-mkfifo ${MOUNT_POINT}/testdir1/pipe1
+    # Create a pipe (FIFO) file
+    mkfifo ${MOUNT_POINT}/testdir1/pipe1
 }
 
 detach_image()
@@ -223,45 +223,46 @@ hdiutil attach ${IMAGE_FILE}.dmg -noautoopen -nobrowse
 
 create_file_entries "/Volumes/hfsplus_test"
 
-BASE_IMAGE_FILE=${IMAGE_FILE}
+detach_image ${IMAGE_FILE}.dmg
 
+# Create compressed UDIF images
 mkdir -p test_data/udif
+
+BASE_IMAGE_FILE=${IMAGE_FILE}
 
 # Create an ADC compressed UDIF image.
 IMAGE_FILE="test_data/udif/hfsplus_adc"
 
 rm -f ${IMAGE_FILE}.dmg
 
-hdiutil create -format UDCO -srcfolder "/Volumes/hfsplus_test" ${IMAGE_FILE}
+hdiutil convert ${BASE_IMAGE_FILE}.dmg -format UDCO -o ${IMAGE_FILE}
 
 # Create a bzip2 compressed UDIF image.
 IMAGE_FILE="test_data/udif/hfsplus_bzip2"
 
 rm -f ${IMAGE_FILE}.dmg
 
-hdiutil create -format UDBZ -srcfolder "/Volumes/hfsplus_test" ${IMAGE_FILE}
+hdiutil convert ${BASE_IMAGE_FILE}.dmg -format UDBZ -o ${IMAGE_FILE}
 
 # Create a lzfse compressed UDIF image.
 IMAGE_FILE="test_data/udif/hfsplus_lzfse"
 
 rm -f ${IMAGE_FILE}.dmg
 
-hdiutil create -format ULFO -srcfolder "/Volumes/hfsplus_test" ${IMAGE_FILE}
+hdiutil convert ${BASE_IMAGE_FILE}.dmg -format ULFO -o ${IMAGE_FILE}
 
 # Create a lzma compressed UDIF image.
 IMAGE_FILE="test_data/udif/hfsplus_lzma"
 
 rm -f ${IMAGE_FILE}.dmg
 
-hdiutil create -format ULMO -srcfolder "/Volumes/hfsplus_test" ${IMAGE_FILE}
+hdiutil convert ${BASE_IMAGE_FILE}.dmg -format ULMO -o ${IMAGE_FILE}
 
 # Create a zlib compressed UDIF image.
 IMAGE_FILE="test_data/udif/hfsplus_zlib"
 
 rm -f ${IMAGE_FILE}.dmg
 
-hdiutil create -format UDZO -srcfolder "/Volumes/hfsplus_test" ${IMAGE_FILE}
-
-detach_image ${BASE_IMAGE_FILE}.dmg
+hdiutil convert ${BASE_IMAGE_FILE}.dmg -format UDZO -o ${IMAGE_FILE}
 
 exit ${EXIT_SUCCESS}
