c58ee83cf81fc05fd158aca658bd6f3436cb7cac
Joachim Metz <joachim.metz@gmail.com>
2026-08-12 11:00:41 +0200

    Worked on support for sparse image (.sparseimage) format

--- scripts/generate_test_data_macos.sh
+++ scripts/generate_test_data_macos.sh
@@ -210,9 +210,11 @@ BASE_IMAGE_FILE=${IMAGE_FILE}
 
 IMAGE_FILE="test_data/sparseimage/hfsplus_aes128"
 
-echo -n KeRaMiCs | hdiutil convert test_data/udif/hfsplus_zlib.dmg -encryption AES-128 -format UDSP -stdinpass -o ${IMAGE_FILE}
+rm -f ${IMAGE_FILE}.sparseimage
 
-# echo -n KeRaMiCs | hdiutil convert test_data/udif/hfsplus_zlib.dmg -encryption AES-128 -format UDSP -stdinpass -o hfsplus_aes128 -tgtimagekey encrypted-encoding-version=1 -o ${IMAGE_FILE}
+echo -n KeRaMiCs | hdiutil convert ${BASE_IMAGE_FILE} -encryption AES-128 -format UDSP -stdinpass -o ${IMAGE_FILE}
+
+# echo -n KeRaMiCs | hdiutil convert ${BASE_IMAGE_FILE} -encryption AES-128 -format UDSP -stdinpass -tgtimagekey encrypted-encoding-version=1 -o ${IMAGE_FILE}
 
 # Create a sparse bundle with a HFS+ file system
 IMAGE_FILE="test_data/sparsebundle/hfsplus"
