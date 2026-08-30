553cdc8e5906d8af5ff5cc1bffc6ab7dc39a190c
Joachim Metz <joachim.metz@gmail.com>
2026-08-30 18:53:56 +0200

    Worked on tests

--- keramics-tools/src/util_tool.rs
+++ keramics-tools/src/util_tool.rs
@@ -389,3 +389,164 @@ fn main() -> ExitCode {
     }
     ExitCode::SUCCESS
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn get_test_data() -> Vec<u8> {
+        vec![
+            0x41, 0x20, 0x63, 0x65, 0x72, 0x61, 0x6d, 0x69, 0x63, 0x20, 0x69, 0x73, 0x20, 0x61,
+            0x6e, 0x79, 0x20, 0x6f, 0x66, 0x20, 0x74, 0x68, 0x65, 0x20, 0x76, 0x61, 0x72, 0x69,
+            0x6f, 0x75, 0x73, 0x20, 0x68, 0x61, 0x72, 0x64, 0x2c, 0x20, 0x62, 0x72, 0x69, 0x74,
+            0x74, 0x6c, 0x65, 0x2c, 0x20, 0x68, 0x65, 0x61, 0x74, 0x2d, 0x72, 0x65, 0x73, 0x69,
+            0x73, 0x74, 0x61, 0x6e, 0x74, 0x2c, 0x20, 0x61, 0x6e, 0x64, 0x20, 0x63, 0x6f, 0x72,
+            0x72, 0x6f, 0x73, 0x69, 0x6f, 0x6e, 0x2d, 0x72, 0x65, 0x73, 0x69, 0x73, 0x74, 0x61,
+            0x6e, 0x74, 0x20, 0x6d, 0x61, 0x74, 0x65, 0x72, 0x69, 0x61, 0x6c, 0x73, 0x20, 0x6d,
+            0x61, 0x64, 0x65, 0x20, 0x62, 0x79, 0x20, 0x73, 0x68, 0x61, 0x70, 0x69, 0x6e, 0x67,
+            0x20, 0x61, 0x6e, 0x64, 0x20, 0x74, 0x68, 0x65, 0x6e, 0x20, 0x66, 0x69, 0x72, 0x69,
+            0x6e, 0x67, 0x20, 0x61, 0x6e, 0x20, 0x69, 0x6e, 0x6f, 0x72, 0x67, 0x61, 0x6e, 0x69,
+            0x63, 0x2c, 0x20, 0x6e, 0x6f, 0x6e, 0x6d, 0x65, 0x74, 0x61, 0x6c, 0x6c, 0x69, 0x63,
+            0x20, 0x6d, 0x61, 0x74, 0x65, 0x72, 0x69, 0x61, 0x6c, 0x2c, 0x20, 0x73, 0x75, 0x63,
+            0x68, 0x20, 0x61, 0x73, 0x20, 0x63, 0x6c, 0x61, 0x79, 0x2c, 0x20, 0x61, 0x74, 0x20,
+            0x61, 0x20, 0x68, 0x69, 0x67, 0x68, 0x20, 0x74, 0x65, 0x6d, 0x70, 0x65, 0x72, 0x61,
+            0x74, 0x75, 0x72, 0x65, 0x2e, 0x0a,
+        ]
+    }
+
+    #[test]
+    fn test_calculate_checksum_with_adler32() -> Result<(), ErrorTrace> {
+        let test_data: Vec<u8> = get_test_data();
+
+        UtilTool::calculate_checksum(&ChecksumType::Adler32, 0x04c11db7, 0, &test_data)?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_calculate_checksum_with_crc32() -> Result<(), ErrorTrace> {
+        let test_data: Vec<u8> = get_test_data();
+
+        UtilTool::calculate_checksum(&ChecksumType::Crc32, 0x04c11db7, 0, &test_data)?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_calculate_checksum_with_fletcher64() -> Result<(), ErrorTrace> {
+        let test_data: Vec<u8> = get_test_data();
+
+        UtilTool::calculate_checksum(&ChecksumType::Fletcher64, 0x04c11db7, 0, &test_data)?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_read_data() -> Result<(), ErrorTrace> {
+        let path: PathBuf = PathBuf::from("../test_data/lzvn/compressed");
+
+        let mut data: Vec<u8> = vec![0; 29];
+        UtilTool::read_data(&path, 0, &mut data)?;
+        assert_eq!(data[0], 0xe0);
+        assert_eq!(data[1], 0x03);
+
+        let mut data: Vec<u8> = vec![0; 3];
+        UtilTool::read_data(&path, 1, &mut data)?;
+        assert_eq!(data[0], 0x03);
+        assert_eq!(data[1], 0x4d);
+        assert_eq!(data[2], 0x79);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_read_data_with_nonexistent_file() {
+        let path: PathBuf = PathBuf::from("../test_data/nonexistent_file");
+
+        let mut data: Vec<u8> = vec![0; 1];
+        let result: Result<(), ErrorTrace> = UtilTool::read_data(&path, 0, &mut data);
+
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_decompress_with_adc() -> Result<(), ErrorTrace> {
+        let compressed_data: Vec<u8> = match std::fs::read("../test_data/adc/compressed") {
+            Ok(data) => data,
+            Err(error) => {
+                return Err(keramics_core::error_trace_new_with_error!(
+                    "Unable to read test data file",
+                    error
+                ));
+            }
+        };
+        UtilTool::decompress(&CompressionType::Adc, &compressed_data)?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_decompress_with_lzn1() -> Result<(), ErrorTrace> {
+        let compressed_data: Vec<u8> = match std::fs::read("../test_data/lznt1/compressed") {
+            Ok(data) => data,
+            Err(error) => {
+                return Err(keramics_core::error_trace_new_with_error!(
+                    "Unable to read test data file",
+                    error
+                ));
+            }
+        };
+        UtilTool::decompress(&CompressionType::Lznt1, &compressed_data)?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_decompress_with_lzvn() -> Result<(), ErrorTrace> {
+        let compressed_data: Vec<u8> = match std::fs::read("../test_data/lzvn/compressed") {
+            Ok(data) => data,
+            Err(error) => {
+                return Err(keramics_core::error_trace_new_with_error!(
+                    "Unable to read test data file",
+                    error
+                ));
+            }
+        };
+        UtilTool::decompress(&CompressionType::Lzvn, &compressed_data)?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_decompress_with_lzxpress() -> Result<(), ErrorTrace> {
+        let compressed_data: Vec<u8> = match std::fs::read("../test_data/lzxpress/compressed") {
+            Ok(data) => data,
+            Err(error) => {
+                return Err(keramics_core::error_trace_new_with_error!(
+                    "Unable to read test data file",
+                    error
+                ));
+            }
+        };
+        UtilTool::decompress(&CompressionType::Lzxpress, &compressed_data)?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_decompress_with_lzxpress_huffman() -> Result<(), ErrorTrace> {
+        let compressed_data: Vec<u8> =
+            match std::fs::read("../test_data/lzxpress_huffman/compressed") {
+                Ok(data) => data,
+                Err(error) => {
+                    return Err(keramics_core::error_trace_new_with_error!(
+                        "Unable to read test data file",
+                        error
+                    ));
+                }
+            };
+        UtilTool::decompress(&CompressionType::LzxpressHuffman, &compressed_data)?;
+
+        Ok(())
+    }
+}
